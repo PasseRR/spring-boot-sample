198dc386dd394252b0c9873e14593e5e05527105
PasseRR <xie__hai@sina.com>
2017-09-01 20:15:27 +0800

    motan server

--- motan-server/src/test/groovy/github/com/passerr/motan/api/impl/UserApiImplSpec.groovy
+++ motan-server/src/test/groovy/github/com/passerr/motan/api/impl/UserApiImplSpec.groovy
@@ -3,6 +3,8 @@ package github.com.passerr.motan.api.impl
 import com.weibo.api.motan.config.springsupport.annotation.MotanReferer
 import github.com.passerr.motan.BaseSpec
 import github.com.passerr.motan.api.UserApi
+import github.com.passerr.motan.vo.UserVo
+
 /**
  * @author xiehai1
  * @date 2017/09/01 18:10
@@ -15,5 +17,35 @@ class UserApiImplSpec extends BaseSpec {
     def motan(){
         expect:
         this.userApi != null
+    }
+
+    def add(){
+        given:
+        def user = new UserVo(loginId: 1)
+
+        when:
+        def response = this.userApi.addUser(user)
+        then:
+        response.isOk()
+        response.getData() == 1
+    }
+
+    def modify(){
+        given:
+        def user = new UserVo(userId: 1)
+
+        when:
+        def response = this.userApi.modifyUser(user)
+        then:
+        response.isOk()
+        response.getData()
+    }
+
+    def delete(){
+        when:
+        def response = this.userApi.deleteUser(2)
+        then:
+        response.isOk()
+        response.getData() == 2
     }
 }
