ca90d39fdcf2915a531cae57342969f467d4f27c
PasseRR <xie__hai@sina.com>
2017-09-01 20:17:43 +0800

    motan server

--- motan-server/src/main/java/github/com/passerr/motan/MotanServerApplication.java
+++ motan-server/src/main/java/github/com/passerr/motan/MotanServerApplication.java
@@ -1,5 +1,7 @@
 package github.com.passerr.motan;
 
+import com.weibo.api.motan.common.MotanConstants;
+import com.weibo.api.motan.util.MotanSwitcherUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,5 +14,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MotanServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(MotanServerApplication.class, args);
+        Runtime.getRuntime()
+            // 优雅停机
+            .addShutdownHook(new Thread(() -> MotanSwitcherUtil.setSwitcherValue(MotanConstants.REGISTRY_HEARTBEAT_SWITCHER, false)));
     }
 }
